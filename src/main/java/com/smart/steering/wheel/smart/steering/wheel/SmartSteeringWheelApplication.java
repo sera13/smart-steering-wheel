01f97671aa017e3d9a3c9db6b2133e2de538b2fa
Serafeim Kourlos <serafeimkourlos@gmail.com>
2023-11-12 22:33:16 +0200

    Commit project structure

---
+++ src/main/java/com/smart/steering/wheel/smart/steering/wheel/SmartSteeringWheelApplication.java
@@ -0,0 +1,13 @@
+package com.smart.steering.wheel.smart.steering.wheel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSteeringWheelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSteeringWheelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart/steering/wheel/smart/steering/wheel/SmartSteeringWheelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart.steering.wheel.smart.steering.wheel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartSteeringWheelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
